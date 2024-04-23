6b2fcc1eb154215dc34f4001bac402b72677bb5e
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-04-23 17:52:06 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,2 @@
+select item_id, item_name, rarity from item_info where item_id in (select item_id from item_tree 
+where parent_item_id in (select item_id from item_info where rarity = 'rare')) order by item_id desc
