54e5567baac50ce23344edd11f76beb7e707f4d0
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-05-21 18:41:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.63 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            answer += A[i] * B[n-i-1];
+        }
+        return answer;
+    }
+}
