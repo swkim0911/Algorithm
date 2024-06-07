80bf6ae44fff43febeb1d9441dc5d94c73907f59
김성원 <swkim@gimseong-won-ui-noteubug.local>
2024-06-07 19:16:30 +0900

    [level 2] Title: 모음 사전, Time: 29.34 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -4,12 +4,11 @@ import java.util.*;
 class Solution {
 
     String[] alphabet = {"A", "E", "I", "O", "U"};
-    Set<String> set = new HashSet<>();
+    List<String> list = new ArrayList<>();
 
     public int solution(String word) {
         int answer = 0;
-        func(0, "");
-        List<String> list = new ArrayList<>(set);
+        func(0, ""); // list에 // "" 도 포함됨
         Collections.sort(list);
         for (int i = 0; i < list.size(); i++) {
             if(list.get(i).equals(word)){
@@ -19,14 +18,10 @@ class Solution {
         }
         return answer;
     }
-
+    
     public void func(int cnt, String word){
-        if(cnt == 5){
-            set.add(word);
-            System.out.println(word);
-            return;
-        }
-        func(cnt+1, word);
+        list.add(word);
+        if(cnt == 5) return;
 
         for(int i = 0; i < alphabet.length; i++){
             func(cnt+1, word+alphabet[i]);
