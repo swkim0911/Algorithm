c1d1a7255c7f1da3ba721134c69e2d1a279ef8ae
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-06-12 09:21:23 +0900

    [level 3] Title: 가장 먼 노드, Time: 41.78 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,47 @@
 import java.util.*;
 
 class Solution {
-    private ArrayList<Integer>[] adj;
-    private int[] visit;
-    private int depth = 0;
+    
+    ArrayList<Integer>[] adj;
+    int[] visited;
+    int depth = 0;
     
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        visit = new int[n+1];
         adj = new ArrayList[n+1];
-        for(int i=1; i<=n; i++) adj[i] = new ArrayList<>();
-        for(int i=0; i<edge.length; i++){
+        visited = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            adj[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < edge.length; i++){
             adj[edge[i][0]].add(edge[i][1]);
             adj[edge[i][1]].add(edge[i][0]);
         }
+        bfs();
         
-        bfs(1, 1);
-        
-        for(int i=1; i<=n; i++){
-            if(depth == visit[i]) answer+=1;
+        for(int i = 1; i <= n; i++){
+            if(depth == visited[i]) answer++;
         }
         
         return answer;
     }
     
-    private void bfs(int start, int count){
-        Queue<Integer> q = new LinkedList();
-        q.add(start);
-        q.add(count);
-        visit[start] = count;
+    public void bfs(){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        visited[1] = 1;
         
         while(!q.isEmpty()){
-            int node = q.poll();
-            int n = q.poll();
+            Integer cur = q.poll();
+            if(depth < visited[cur]) depth = visited[cur];
             
-            if(depth < n) depth = n;
-            for(int i=0; i<adj[node].size(); i++){
-                int next = adj[node].get(i);
-                
-                if(visit[next] != 0) continue;
-                visit[next] = n+1;
-                q.add(next);
-                q.add(n+1);
+            for(int i = 0; i < adj[cur].size(); i++){
+                Integer nxt = adj[cur].get(i);
+                if(visited[nxt] != 0) continue;
+                visited[nxt] = visited[cur]+1;
+                q.add(nxt);
             }
         }
     }
+    
 }
