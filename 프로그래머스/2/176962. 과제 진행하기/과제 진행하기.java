a8e6a27c98c6926d74aa1c42b64f219f2d61d9c2
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-06-26 17:21:46 +0900

    [level 2] Title: 과제 진행하기, Time: 10.14 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[][] plans) {
+        //init
+        List<String> answer = new ArrayList<>(); // 정답
+        PriorityQueue<Plan> pq = new PriorityQueue<>();
+        Stack<Plan> stack = new Stack<>(); // 중지된 plan stack
+        for(String[] plan : plans){
+            pq.add(new Plan(plan[0], plan[1], plan[2]));
+        }
+        Plan cur = pq.poll();
+        int curTime = cur.start;
+        boolean flag = true;
+        while(flag){
+            
+            int minTime = 0;
+            boolean isProcessing = true;
+            
+            while(true){
+                
+                // 처리가 완료되는 경우
+                if(minTime == cur.playTime){
+                    answer.add(cur.name);
+                    isProcessing = false;
+                    
+                    // 신규 과제가 있는 경우
+                    if(!pq.isEmpty() && curTime == pq.peek().start){
+                        cur = pq.poll();
+                        curTime += minTime;
+                        break;
+                    }
+                    
+                    if(!stack.isEmpty()){
+                        cur = stack.pop();
+                        curTime += minTime;
+                        break;
+                    }
+                    
+                    if(pq.isEmpty() && stack.isEmpty()){
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if(!pq.isEmpty() && (curTime + minTime) == pq.peek().start){
+                    if(isProcessing){
+                        cur.playTime -= minTime;
+                        stack.add(cur);
+                    }
+                    cur = pq.poll();
+                    curTime += minTime;
+                    break;
+                }
+                
+                minTime++;
+            }
+        }
+            
+        return answer.toArray(new String[0]);
+    }
+}
+class Plan implements Comparable<Plan>{
+    String name;
+    int start;
+    int playTime;
+    
+    public Plan(String name, String startTime, String playTime){
+        this.name = name;
+        this.start = Integer.parseInt(startTime.split(":")[0])*60 + Integer.parseInt(startTime.split(":")[1]);
+        this.playTime = Integer.parseInt(playTime);
+    }
+    
+    
+    @Override
+    public int compareTo(Plan o){
+        return this.start - o.start;
+    }
+}
