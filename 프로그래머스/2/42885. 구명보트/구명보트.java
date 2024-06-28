e7a2535c0cbbf42a2853d0f4e8526898c4650db4
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-06-28 15:40:06 +0900

    [level 2] Title: 구명보트, Time: 9.34 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] people, int limit) {
+        // init
+        int answer = 0;
+        int[] weights = new int[241]; //weights[x]: 몸무게가 x인 사람수 (x: 40kg ~ 240kg)
+        for(int p : people){
+            weights[p] += 1; // 몸무게 p인 사람 + 1
+        }
+        for(int i = 240; i >= 0; i--){
+            while(weights[i] != 0){
+                weights[i]--;
+                answer++; 
+                for(int j = limit - i; j >= 0; j--){
+                    if(weights[j] > 0){ // limit 안으로 두명이 탈 수 있을 경우
+                        weights[j]--;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
