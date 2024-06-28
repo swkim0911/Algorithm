e347b0aa4d456c4fe22f387374da6dfdfb89aaa7
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-06-28 21:49:01 +0900

    [level 1] Title: 덧칠하기, Time: 1.30 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,11 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        boolean[] visited = new boolean[n+1];
-        for(int i = 0; i < section.length; i++){
-            visited[section[i]] = true;
-        }
-        
-        for(int i = 0; i < section.length; i++){
-            if(visited[section[i]]){
+        int cursor = 0; // 페인트칠 한 마지막 위치
+        for(int number : section){
+            if(cursor < number){
+                cursor = number + m - 1; // number ~ number + m 범위까지 페인트 칠
                 answer++;
-                for(int j = section[i]; j < section[i] + m; j++){
-                    if(j <= n && visited[j]) visited[j] = false;
-                }
             }
         }
         
