3769db31fb1325eabfdf467ee7bc89c9dbe53d6e
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-06-30 21:44:14 +0900

    [level 4] Title: 도둑질, Time: 27.86 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] money) {
+       int[][] dp = new int[2][money.length]; // dp[0][x]: 첫집 방문o. x까지 최댓값, dp[1][x]: 첫집 방문x. x까지 최댓값
+        dp[0][0] = money[0];
+        dp[1][0] = 0;
+        
+        dp[0][1] = money[0];
+        dp[1][1] = money[1];
+        
+        for(int i = 2; i < money.length; i++){
+            dp[0][i] = Math.max(dp[0][i-1], money[i] + dp[0][i-2]);
+            dp[1][i] = Math.max(dp[1][i-1], money[i] + dp[1][i-2]);
+        }
+        
+        return Math.max(dp[1][money.length-1], dp[0][money.length-2]);
+    }
+}
