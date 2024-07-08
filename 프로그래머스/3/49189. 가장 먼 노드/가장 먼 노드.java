029cbea677cd28d46e7717c281771329a340c0b8
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-07-08 16:38:20 +0900

    [level 3] Title: 가장 먼 노드, Time: 35.98 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,46 +2,41 @@ import java.util.*;
 
 class Solution {
     
-    ArrayList<Integer>[] adj;
-    int[] visited;
-    int depth = 0;
+    List<Integer>[] graph = new ArrayList[20_001];
+    int[] visited = new int[20_001];
+    int max = Integer.MIN_VALUE;
     
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        adj = new ArrayList[n+1];
-        visited = new int[n+1];
-        for(int i = 1; i <= n; i++){
-            adj[i] = new ArrayList<>();
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
         }
-        for(int i = 0; i < edge.length; i++){
-            adj[edge[i][0]].add(edge[i][1]);
-            adj[edge[i][1]].add(edge[i][0]);
+        for(int[] e : edge){
+            int a = e[0];
+            int b = e[1];
+            graph[a].add(b);
+            graph[b].add(a);
         }
         bfs();
-        
-        for(int i = 1; i <= n; i++){
-            if(depth == visited[i]) answer++;
+        for(int i = 1; i < visited.length; i++){
+            if(max == visited[i]) answer++;
         }
-        
         return answer;
     }
     
     public void bfs(){
         Queue<Integer> q = new LinkedList<>();
-        q.add(1);
+        q.add(1); // 시작
         visited[1] = 1;
-        
         while(!q.isEmpty()){
             Integer cur = q.poll();
-            if(depth < visited[cur]) depth = visited[cur];
-            
-            for(int i = 0; i < adj[cur].size(); i++){
-                Integer nxt = adj[cur].get(i);
-                if(visited[nxt] != 0) continue;
-                visited[nxt] = visited[cur]+1;
+            if(max < visited[cur]) max = visited[cur];
+            for(int nxt : graph[cur]){
+                if(visited[nxt] > 0) continue; // 방문한 경우
+                visited[nxt] = visited[cur] + 1;
                 q.add(nxt);
             }
         }
     }
-    
 }
+// bfs
