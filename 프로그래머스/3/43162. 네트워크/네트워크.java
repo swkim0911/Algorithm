332cd087cee6ea3544d7c458d30da2bfa3c5add4
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-09-30 18:04:03 +0900

    [level 3] Title: 네트워크, Time: 1.06 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,46 @@
 import java.util.*;
 
 class Solution {
+    
+    ArrayList<Integer>[] graph;
     boolean[] isVisited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        graph = new ArrayList[n];
         isVisited = new boolean[n];
         for(int i = 0; i < n; i++){
-            if(isVisited[i]) continue;
-            bfs(i, n, computers);
-            answer++;
+            graph[i] = new ArrayList<>();
+            
+            for(int j = 0; j < n; j++){
+                if(i != j && computers[i][j] == 1){
+                    graph[i].add(j);
+                }
+            }
         }
+        
+        for(int i = 0; i < n; i++){
+            if(!isVisited[i]){
+                bfs(i);
+                answer++;
+            }
+        }
+        
         return answer;
     }
     
-    public void bfs(int start, int n, int[][] computers){
+    // bfs
+    public void bfs(int start){
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
         isVisited[start] = true;
         while(!q.isEmpty()){
-            Integer cur = q.poll();
-            for(int i = 0; i < n; i++){
-                if(i == cur || isVisited[i]) continue;
-                if(computers[cur][i] == 1){
-                    isVisited[i] = true;
-                    q.add(i);
-                }
+            int cur = q.poll();
+            for(int nxt : graph[cur]){
+                if(isVisited[nxt]) continue;
+                q.add(nxt);
+                isVisited[nxt] = true;
             }
         }
     }
 }
-
-// bfs
