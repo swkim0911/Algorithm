fd887e2d9fdf0d4f08c433dbd66f983764a4115a
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-10-11 22:29:04 +0900

    [level 2] Title: 주식가격, Time: 43.57 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,23 +4,19 @@ class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
+        Stack<Integer> stack = new Stack<>(); // stack에 남아있는 index들은 아직 가격이 떨어지지 않은 곳을 만나지 못했다.
         for(int i = 0; i < n; i++){
-            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){ // i 번째에서 가격이 떨어지는 순간
                 answer[stack.peek()] = i - stack.peek();
                 stack.pop();
             }
             stack.add(i);
         }
-        // 0 1 2 4
-        // answer[3] = 1;
-        while(!stack.isEmpty()){
+        
+        while(!stack.isEmpty()){ // prices 배열을 끝까지 봤을 때 떨어지는 순간을 만나지 못해 stack에 남아있는 경우
             answer[stack.peek()] = n - stack.peek() - 1;
             stack.pop();
         }
         return answer;
     }
 }
-// 3 2
-// 부르트포스를 피하기 위해 stack 사용
