47ebd1b9eb3b99ca99cb32b3e94fb6e4cf5f57bc
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-10-16 15:38:47 +0900

    [Gold II] Title: 보석 도둑, Time: 1336 ms, Memory: 126940 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -10,37 +8,32 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] jewels = new int[n][2];
-
+        int[][] jewels = new int[n][2]; // jewels[x][0]: x번째 보석 무게, jewels[x][1]: x번째 보석 가격
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 2; j++){
-                jewels[i][j] = Integer.parseInt(st.nextToken());
-            }
+            jewels[i][0] = Integer.parseInt(st.nextToken());
+            jewels[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        TreeMap<Integer, Integer> tMap = new TreeMap<>(); // <가방 최대 무게, 중복 개수>
+        Arrays.sort(jewels, (o1,o2) -> o2[1] - o1[1]); // 가격을 기준으로 내림차순 정렬
+        TreeMap<Integer, Integer> tMap = new TreeMap<>();
         for(int i = 0; i < k; i++){
             int weight = Integer.parseInt(br.readLine());
             tMap.put(weight, tMap.getOrDefault(weight, 0) + 1);
         }
 
-        Arrays.sort(jewels, (o1,o2) -> o2[1] - o1[1]); // 비싼게 앞으로(내림차순 정렬)
-
         long answer = 0;
-        for(int i = 0; i < n; i++){
-            int[] jewel = jewels[i];
-            Integer bag = tMap.ceilingKey(jewel[0]); // jewel의 무게보다 큰 것 중 가장 작은 무게, 또는 같은 무게
+
+        for(int[] jewel : jewels){
+            Integer bag = tMap.ceilingKey(jewel[0]);
             if(bag != null){
                 answer += jewel[1];
-                int cnt = tMap.get(bag); // 중복 개수
+                int cnt = tMap.get(bag);
                 if(cnt > 1){
                     tMap.replace(bag, cnt - 1);
                 }else{
                     tMap.remove(bag);
                 }
             }
-
         }
         System.out.print(answer);
     }
