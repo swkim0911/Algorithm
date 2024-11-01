d06bbe852714262cdbc3d4d90564dffc6047ffa0
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2024-11-01 11:02:24 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,4 +1,4 @@
 select id, email, first_name, last_name
 from developer_infos
-where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+where 'python' in (skill_1, skill_2, skill_3)
 order by 1
