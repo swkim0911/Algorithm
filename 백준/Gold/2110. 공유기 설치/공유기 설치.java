573ed453ff2340ef32b5a73585216bdf24edcd0c
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-01-08 20:13:57 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 29504 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int c = Integer.parseInt(split[1]);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int left = 1;
+        int right = arr[n - 1];
+        int ans = 0;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            int i = 0;
+            int cur = arr[i];
+            int cnt = 1; // 설치된 공유기 개수
+            while(i < n){
+                if(arr[i] - cur >= mid){
+                    cur = arr[i];
+                    cnt++;
+                }
+                i++;
+            }
+            if(cnt < c){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+                ans = mid;
+            }
+        }
+        System.out.println(ans);
+    }
+}
