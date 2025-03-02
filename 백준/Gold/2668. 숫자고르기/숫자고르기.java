c2a9d9454e5d2428e20238969cc62c9095508665
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-03-02 16:17:58 +0900

    [Gold V] Title: 숫자고르기, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static boolean[] visited; // 묶음이 생긴 정수
+    static int[] graph;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        graph = new int[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int v = Integer.parseInt(br.readLine());
+            graph[i] = v;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if(visited[i]) continue;
+            dfs(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if(visited[i]){
+                cnt++;
+                sb.append(i).append("\n");
+            }
+        }
+        sb.insert(0, cnt + "\n");
+        System.out.print(sb.toString().trim());
+    }
+
+    public static void dfs(int node) {
+        boolean[] v = new boolean[n + 1]; // 첫째 줄 방문 유무
+        Set<Integer> top = new HashSet<>();
+        Set<Integer> bottom = new HashSet<>();
+        while (!v[node] && !visited[node]) {
+            v[node] = true;
+            top.add(node);
+            node = graph[node];
+            bottom.add(node);
+        }
+        if (top.size() == bottom.size()) {
+            boolean flag = true;
+            for (Integer num : top) {
+                if(!bottom.contains(num)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                for(Integer num : top){
+                    visited[num] = true;
+                }
+            }
+        }
+
+    }
+}
+// 1 2 3 4
+// 2 3 4 2
+//
