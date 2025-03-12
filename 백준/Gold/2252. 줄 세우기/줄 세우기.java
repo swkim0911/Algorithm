a9f3a65dd0ec31a2e923bd242facf406f6bffd08
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-03-12 09:28:57 +0900

    [Gold III] Title: 줄 세우기, Time: 464 ms, Memory: 46560 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -10,43 +11,39 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        ArrayList<Integer>[] graph = new ArrayList[n+1]; // graph[i].get(j): i번째 노드에서 j번째 노드로 방향을 갖는 엣지
-
-        for(int i = 0; i <= n; i++){
+        ArrayList<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
-
-        int[] indegree = new int[n+1]; // indegree[i]: i번째 노드로 방향이 향하는 엣지 수
-
-        while(m-- >0){
+        int[] indegree = new int[n + 1];
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            graph[from].add(to);
             indegree[to]++;
+            graph[from].add(to);
         }
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>(); // fifo니까 순서를 지킨다. 큐에 들어간 번호는 중복되서 들어갈 수 없다.
         ArrayList<Integer> result = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             if(indegree[i] == 0) q.add(i);
         }
 
         while (!q.isEmpty()) {
-            int cur = q.poll();
+            Integer cur = q.poll();
             result.add(cur);
             for(int nxt : graph[cur]){
                 indegree[nxt]--;
                 if(indegree[nxt] == 0) q.add(nxt);
             }
         }
-
         StringBuilder sb = new StringBuilder();
-        for(int val : result){
-            sb.append(val).append(" ");
+        for (int v : result) {
+            sb.append(v).append(" ");
         }
-
-        System.out.println(sb.toString().trim());
+        System.out.print(sb);
     }
 }
+// 내 이후에 아무것도 넣을 필요 없으면 뽑기
+// 1 -> 3, 2 -> 3  3입장에서 내 앞에1,2 가 있어야 함.
 
