82640fd97911325087f2e1a3b09816f22c11ca10
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-03-29 08:36:08 +0900

    [level 0] Title: 정수 찾기, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.py
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.py
@@ -1,8 +1,3 @@
 def solution(num_list, n):
-    answer = 0
-    for x in num_list:
-        if x == n:
-            answer = 1
-            break
-            
-    return answer
+
+    return int(n in num_list)
