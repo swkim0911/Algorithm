be20ae4e78ee28d794afdd7633c5c74413c0b4c1
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-03-30 09:11:49 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,11 +1,5 @@
 def solution(arr, queries):
-    answer = []
-    for q in queries:
-        i = q[0]
-        j = q[1]
-        # swap
-        tmp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = tmp
+    for i,j in queries:
+        arr[i], arr[j] = arr[j], arr[i]
         
     return arr
