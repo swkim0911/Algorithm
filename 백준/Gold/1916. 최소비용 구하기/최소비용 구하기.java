fce071f3f3a103582984f2dd41c6d1975a6f4912
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-05-02 14:02:31 +0900

    [Gold V] Title: 최소비용 구하기, Time: 400 ms, Memory: 50300 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,60 +1,66 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
 
-    static final int INF = Integer.MAX_VALUE / 2;
+    public static final int INF = Integer.MAX_VALUE / 2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
         ArrayList<Pair>[] graph = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            graph[s].add(new Pair(e, d));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[a].add(new Pair(b, c));
         }
+
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
-        PriorityQueue<Pair> pq = new PriorityQueue<>((o1, o2) -> o1.d - o2.d);
+
+        int[] dist = new int[n + 1]; // dist[x]: 출발지에서 x까지 최단거리
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((o1, o2) -> o1.node - o2.node);
         pq.add(new Pair(start, 0));
-        int[] distance = new int[n + 1]; // distance[x]: start에서 x까지 최소 거리
-        Arrays.fill(distance, INF);
-        distance[start] = 0;
 
         while (!pq.isEmpty()) {
             Pair cur = pq.poll();
-            if(distance[cur.v] < cur.d) continue;
+            if(dist[cur.node] < cur.dist) continue;
 
-            for(Pair nxt : graph[cur.v]){
-                int nextDistance = cur.d + nxt.d;
-                if(nextDistance < distance[nxt.v]){
-                    distance[nxt.v] = nextDistance;
-                    pq.add(new Pair(nxt.v, nextDistance));
+            for(Pair next : graph[cur.node]){
+                int nextDistance = cur.dist + next.dist; // 현재 노드를 거쳐서 next 노드까지 가는 거리
+                if (nextDistance < dist[next.node]) {
+                    dist[next.node] = nextDistance;
+                    pq.add(new Pair(next.node, nextDistance));
                 }
             }
         }
 
-        System.out.println(distance[end]);
+        int answer = dist[end];
+        System.out.println(answer);
+    }
+
+    static class Pair{
+        int node;
+        int dist;
+
+        public Pair(int node, int dist){
+            this.node = node;
+            this.dist = dist;
+        }
     }
 }
 
-class Pair{
-    int v; // vertex
-    int d; // distance
 
-    public Pair(int v, int d){
-        this.v = v;
-        this.d = d;
-    }
-
-}
 
