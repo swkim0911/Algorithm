d7d38fd2200f7d88b08fc2c2aa2d2e8518d03ab0
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-06-30 14:05:05 +0900

    [Silver III] Title: 크로스 컨트리, Time: 72 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,61 +1,40 @@
 import sys
-from collections import Counter
+from collections import Counter, defaultdict
 
 input = sys.stdin.readline
 
 t = int(input().strip())
 
-string_builder = ''
+result = []
 
 for _ in range(t):
-    n = int(input().strip())
-    teams = list(map(int, input().strip().split(' ')))
+    n = int(input())
+    teams = list(map(int, input().split(' ')))
     counter = Counter(teams)  # 각 팀 당 몇명이 참가하는지 기록된 dict
-    failed_teams = []  # 6명이 안되는 team들
-    for k, v in counter.items():
-        if v != 6:
-            failed_teams.append(k)
+    valid_teams = {team for team, count in counter.items() if count == 6}
     score = 1
-    d = {}  # t:[x,y,z] -> t팀의 정보. [현재까지 팀 인원, 4번까지 합, 5번의 점수]
+    d = defaultdict(lambda: [0, 0, 0])  # [참가자 수, 1~4위 합계, 5위 점수]
     for member in teams:
-        if member in failed_teams:
+        if member not in valid_teams:
             continue
-        if member in d:
-            info = d[member]
-            info[0] += 1
-            if info[0] < 5:
-                info[1] += score
-            elif info[0] == 5:
-                info[2] = score
 
-        else:  # 팀에서 첫번째인 경우
-            info = [0] * 3
-            info[0] += 1
+        info = d[member]
+        info[0] += 1
+        if info[0] < 5:
             info[1] += score
-            d[member] = info
+        elif info[0] == 5:
+            info[2] = score
 
         score += 1
 
     # 가장 낮은 점수 찾기
-    min_value = float('inf')
-    for info in d.values():
-        if info[1] < min_value:
-            min_value = info[1]
+    min_sum = min(info[1] for info in d.values())
 
     # 가장 낮은 점수를 가진 팀 후보 찾기
-    candidators = []
-    for k, info in d.items():
-        if info[1] == min_value:
-            candidators.append(k)
+    candidates = [team for team, info in d.items() if info[1] == min_sum]
 
-    min_answer = float('inf')
-    answer = 0
-    # 후보중 5번째 주자가 가장 빨리 들어온 팀 선택
-    for c in candidators:
-        if d[c][2] < min_answer:
-            min_answer = d[c][2]
-            answer = c
+    answer = min(candidates, key=lambda team: d[team][2])
 
-    string_builder += str(answer) + '\n'
+    result.append(str(answer))
 
-print(string_builder)
+print('\n'.join(result))
