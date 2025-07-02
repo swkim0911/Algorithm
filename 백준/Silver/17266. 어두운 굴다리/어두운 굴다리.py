425ba9da90f9b617535c9ff96a597e6dae48c21b
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-07-02 14:47:39 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 208 ms, Memory: 42660 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -0,0 +1,40 @@
+import sys
+
+input = sys.stdin.readline
+
+# 모든 굴다리를 밝힐 수 있는지 확인하는 함수
+
+
+def is_possible(h, positions):
+
+    if h - positions[0] < 0:
+        return False
+
+    for i in range(1, m):
+        if positions[i] - positions[i-1] > 2 * h:
+            return False
+    if n - positions[-1] > h:
+        return False
+
+    return True
+
+
+n = int(input())
+m = int(input())
+positions = list(map(int, input().split(' ')))
+
+left = 1
+right = n
+
+answer = n
+
+while left < right:
+    mid = (left + right) // 2
+
+    if is_possible(mid, positions):
+        right = mid
+    else:
+        left = mid + 1
+
+
+print(right)
