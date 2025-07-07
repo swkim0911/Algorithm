4bd035b4d4eef871667cb160a91ccb1b6a31f138
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-07-07 15:52:39 +0900

    [Silver III] Title: 햄버거 분배, Time: 76 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split(' '))
+
+arr = list(input())
+visited = [False] * n
+
+for i, v in enumerate(arr):
+    if v == 'P':
+        s = i - k if i - k >= 0 else 0
+        e = i + k if i + k < n else n - 1
+        for nxt in range(s, e + 1):
+            if nxt == i:
+                continue
+            if arr[nxt] == 'H' and not visited[nxt]:
+                visited[nxt] = True
+                break
+
+answer = 0
+for v in visited:
+    if v:
+        answer += 1
+
+print(answer)
