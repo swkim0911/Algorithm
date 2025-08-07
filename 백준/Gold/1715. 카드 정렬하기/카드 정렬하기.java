91d439f53e66f51886fc90091cbf803865c8cab4
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-08-07 14:37:22 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 320 ms, Memory: 25456 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 public class Main {
 
@@ -11,33 +11,16 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
-            int bundle = Integer.parseInt(br.readLine());
-            pq.add(bundle);
+            pq.add(Integer.parseInt(br.readLine()));
         }
-        long ans = 0;
+        int answer = 0;
         while (pq.size() > 1) {
-            int sum = 0;
-            for (int i = 0; i < 2; i++) {
-                int min = pq.poll();
-                sum += min;
-            }
-            ans += sum;
-            pq.add(sum);
+            Integer first = pq.poll();
+            Integer second = pq.poll();
+            answer += first + second;
+            pq.add(first + second);
         }
-        System.out.println(ans);
-        
-        /**
-         * a + b
-         * a + b + c
-         * a + b + c + d
-         * = 3a + 3b + 2c + d
-         */
-
-        /**
-         * a + b
-         * c + d
-         * a + b + c + d
-         * = 2a + 2b + 2c + 2d
-         */
+        System.out.println(answer);
     }
+
 }
