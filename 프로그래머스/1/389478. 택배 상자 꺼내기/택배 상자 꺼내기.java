58922f63a53fc370cc5108250b794572038c6206
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-08-14 10:44:11 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,39 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int w, int num) {
-        ArrayList<int[]> board = new ArrayList<>();
-        int t = 0;
-        int box = 1;
-        while(n != 0){
-            int[] row = new int[w];
-            for(int i = 0; i < (w < n ? w : n); i++){
-                row[i] = box++;
-            }
-            if(t % 2 != 0){ // 뒤집기
-                for(int i = 0; i < row.length / 2; i++){
-                    int tmp = row[i];
-                    row[i] = row[row.length - i - 1];
-                    row[row.length - i - 1] = tmp;
-                }
-            }
-            
-            board.add(row);
-            n = w < n ? n-w : 0;
-            t += 1;
-        }
         int answer = 0;
-        
-        int col = -1;
-        
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < w; j++){
-                if(board.get(i)[j] == num){
-                    col = j;
-                }
-                if(col != -1 && j == col && board.get(i)[j] != 0){
-                    answer += 1;
-                }
+        int r = (num - 1) / w; // num의 x좌표
+        int c = (r % 2 == 0 ? (num - 1) % w : (w - 1) - ((num - 1) % w)); // num의 y좌표
+        for(; r < (n + (w-1)) / w; r++){
+            int cur = (w * r) + (r % 2 == 0 ? (c + 1) : (w + 1) - (c + 1));
+            if(cur <= n){
+                answer += 1;
             }
         }
         
@@ -41,13 +14,11 @@ class Solution {
     }
 }
 
-// 1 2 3 4 5
-// 5 4 3 2 1
+// 수학으로 풀기
+// 1. num의 위치 r,c 을 찾고
+// 2. 그 위에 수들을 직접 찾아보면서 개수 찾기. 단, <= n 
 
-/**
-1 2 3
-6 5 4
-7 8 9
-12 11 10
-13
-**/
+// 1. w의 위치
+// r = (num - 1) / w
+// c = r % 2 == 0 ? (num - 1) % w : (w - 1) - (num - 1) % w
+
