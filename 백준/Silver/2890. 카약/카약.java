119bb6fd0659075a8b8c5590d72d65860a9bfe65
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-08-16 17:55:18 +0900

    [Silver V] Title: 카약, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/2890. 카약/카약.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] ranks = new int[10];
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+        int rank = 1;
+        char[][] board = new char[row][col];
+        for (int i = 0; i < row; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
+
+        for (int j = col - 2; j >= 1; j--) {
+            boolean flag = false;
+            for (int i = 0; i < row; i++) {
+                char c = board[i][j];
+                if((c >= '1' && c <= '9') && ranks[c - '0'] == 0){
+                    ranks[c - '0'] = rank;
+                    flag = true;
+                }
+            }
+
+            if(flag) rank += 1;
+        }
+        for (int i = 1; i < ranks.length; i++) {
+            System.out.println(ranks[i]);
+        }
+    }
+}
