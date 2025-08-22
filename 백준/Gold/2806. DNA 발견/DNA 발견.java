4b5735b676b87c83e8fc0d71674dbd4f76f6803b
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-08-22 14:47:25 +0900

    [Gold IV] Title: DNA 발견, Time: 272 ms, Memory: 46384 KB -BaekjoonHub

--- 백준/Gold/2806. DNA 발견/DNA 발견.java
+++ 백준/Gold/2806. DNA 발견/DNA 발견.java
@@ -8,11 +8,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-
-        int[][] dp = new int[n][2]; //dp[x][0]: 모두 a로 만드는 최소 돌연변이 횟수 dp[x][1]: 모두 b로 만드는 최소 돌연변이 횟수
-        // 초기화
-        if(str.charAt(0) == 'A'){
+        String s = br.readLine();
+        int[][] dp = new int[n][2]; // dp[i][0]: index i까지 모두 A로 만들 때 경우의 수, dp[i][1]: index i까지 모두 B로 만들 때 경우의 수
+        if(s.charAt(0) == 'A') {
             dp[0][0] = 0;
             dp[0][1] = 1;
         }else{
@@ -20,18 +18,16 @@ public class Main {
             dp[0][1] = 0;
         }
         for (int i = 1; i < n; i++) {
-            char c = str.charAt(i);
+            char c = s.charAt(i);
             if(c == 'A'){
                 dp[i][0] = Math.min(dp[i-1][0], dp[i-1][1] + 1);
-                dp[i][1] = Math.min(dp[i-1][0], dp[i-1][1]) + 1;
+                dp[i][1] = Math.min(dp[i-1][0] + 1, dp[i-1][1] + 1);
             }else{
-                dp[i][0] = Math.min(dp[i-1][0], dp[i-1][1]) + 1;
+                dp[i][0] = Math.min(dp[i-1][0] + 1, dp[i-1][1] + 1);
                 dp[i][1] = Math.min(dp[i-1][0] + 1, dp[i-1][1]);
             }
         }
         int answer = Math.min(dp[n-1][0], dp[n-1][1] + 1);
         System.out.println(answer);
-
-
     }
 }
