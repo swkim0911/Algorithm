92fb0a0b7b1d4c92b31d34c8a64ec8a564cf2323
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-09-15 15:04:05 +0900

    [Silver I] Title: 동물원, Time: 116 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n+1][3];
+        // 초기화
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901;
+        }
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += dp[n][i];
+        }
+        System.out.println(sum % 9901);
+    }
+}
