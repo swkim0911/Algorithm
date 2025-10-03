a6813849bb723960bd46ddab1ff2dfd888364f71
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-10-03 10:42:28 +0900

    [level 3] Title: 주사위 고르기, Time: 7116.43 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,75 +1,69 @@
 from itertools import combinations
-import math
 
 def solution(dice):
+    answer = []
     n = len(dice)
     idx_set = set(range(n))
     
-    # 1. 주사위를 선택할 조합
-    answer_arr = []
     max_wins = -1
     
-    for combination in combinations(range(n), n // 2):
-        a = set(combination)
-        b = idx_set - a
-        
-        a = list(a)
-        b = list(b)
-        a_arr = [] # a가 가능한 모든 조합
-        b_arr = [] # b가 가능한 모든 조합
+   
+    for comb in combinations(range(n), n // 2):
+        # 1. a와 b가 선택할 수 있는 조합
+        a_comb = list(comb)
+        b_comb = list(idx_set - set(comb))
     
-        # 2. a 주사위 값의 모든 경우 (오름차순 정렬)
-        for v in range(6**(n//2)):
-            val = v
-            total = 0
-            for i in range(n//2):
-                rest = val % 6
-                total += dice[a[i]][rest]
-                val //= 6
-            a_arr.append(total)
-        # 3. b 주사위 값의 모든 경우 (오름차순 정렬)
+        # 2. a의 주사위 조합에서 가능한 모든 경우 6^(n//2)    
+        # 3. b의 주사위 조합에서 가능한 모든 경우
+        a_results = []
+        b_results = []
+        for i in range(6**(n//2)):
+            cur = i
+            a_total = 0
+            b_total = 0
+            for j in range(n//2):
+                rest = cur % 6
+                a_total += dice[a_comb[j]][rest]
+                b_total += dice[b_comb[j]][rest]
+                cur //= 6
+                
+            a_results.append(a_total)
+            b_results.append(b_total)
         
-        for v in range(6**(n//2)):
-            val = v
-            total = 0
-            for i in range(n//2):
-                rest = val % 6
-                total += dice[b[i]][rest]
-                val //= 6
-            b_arr.append(total)
         
-        a_arr.sort()
-        b_arr.sort()
+        # 오름차순 정렬
+        a_results.sort()
+        b_results.sort()
         
-        total_wins = 0
-        for v in a_arr:
-            wins = binary_search(v, b_arr)
-            total_wins += wins
-        
-        if max_wins < total_wins:
-            max_wins = total_wins
-            answer_arr = a
-        
-    return [v + 1 for v in answer_arr]
-            
-# b_arr에서 val보다 같거나 커지는 구간 찾기 (val이 이기는 개수)
-def binary_search(val, b_arr):
+        a_wins = 0
+        # a의 각 결과에 대해 바이너리 서치로 얼마큼 이길 수 있는지 확인한다 (log n)
+        for a_result in a_results:
+            a_win = binary_search(a_result, b_results)
+            a_wins += a_win
+        if a_wins > max_wins:
+            max_wins = a_wins
+            answer = a_comb
+    
+    answer = [x + 1 for x in answer]
+
+    return answer
+
+
+# a_result보다 작지 않는 첫번쨰 index찾기
+def binary_search(a_result, b_results):
+    n = len(b_results)
     left = 0
-    right = len(b_arr) - 1
+    right = n - 1
     while left < right:
-        mid = (left + right) // 2
-        if b_arr[mid] >= val:
+        mid = (left + right) // 2;
+        if b_results[mid] >= a_result:
             right = mid
         else:
             left = mid + 1
-            
-    if right == (len(b_arr) - 1) and val > b_arr[-1]:
+    if right == n - 1:
+        if a_result > b_results[-1]:
             return right + 1
-    
+        
     return right
-    
 
-        
-        
-        
         
