01757d237cbc4ebe672a540bb1517324143664cd
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-10-05 15:01:40 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 375.12 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,17 +1,13 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    delivery = 0 # delivery < 0: 배달 공간 여유가 있는거
-    pickup = 0 # pickup < 0: 수거 공간 여유가 있는거
+    remain_delivery = 0
+    remain_pickup = 0
     for i in range(n-1, -1, -1):
-        delivery += deliveries[i]
-        pickup += pickups[i]
+        remain_delivery += deliveries[i]
+        remain_pickup += pickups[i]
         
-        while delivery > 0 or pickup > 0:
-            delivery -= cap
-            pickup -= cap
-            
+        while remain_delivery > 0 or remain_pickup > 0:
+            remain_delivery -= cap
+            remain_pickup -= cap
             answer += (i+1) * 2
-    
     return answer
-
-# 그리디
