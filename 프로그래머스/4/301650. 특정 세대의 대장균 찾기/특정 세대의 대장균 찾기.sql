bcaee40391c400af79cf1daf0b55f29d0db9651e
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2026-04-06 14:53:47 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,8 @@
+select id
+from ecoli_data
+where parent_id in (select id
+                        from ecoli_data
+                        where parent_id in (select id from ecoli_data where parent_id is null))
+order by id
+
+                                    
