fced3189d66fde18353e39d4d7b42cafbb7c2942
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2026-04-06 15:50:28 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,31 +1,26 @@
-WITH RECURSIVE ecoli_tree AS (
-    -- 1. 루트 (1세대)
-    SELECT 
-        ID,
-        PARENT_ID,
-        1 AS GENERATION
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IS NULL
-
-    UNION ALL
-
-    -- 2. 자식으로 확장
-    SELECT 
-        e.ID,
-        e.PARENT_ID,
-        t.GENERATION + 1
-    FROM ECOLI_DATA e
-    JOIN ecoli_tree t
-        ON e.PARENT_ID = t.ID
+# 1. 모든 id 들에 대해서 각각 몇 세대인지
+with recursive ecoli_tree as (
+    select 
+        id,
+        parent_id,
+        1 as generation
+    from ecoli_data
+    where parent_id is null
+    
+    union all
+    
+    select 
+        e.id,
+        e.parent_id,
+        r.generation + 1 as generation
+    from ecoli_data e
+    join ecoli_tree r
+        on e.parent_id = r.id
 )
 
--- 3. leaf만 필터링 + 세대별 집계
-SELECT 
-    COUNT(*) AS COUNT,
-    t.GENERATION
-FROM ecoli_tree t
-LEFT JOIN ECOLI_DATA c
-    ON t.ID = c.PARENT_ID
-WHERE c.ID IS NULL   -- 자식 없는 애들
-GROUP BY t.GENERATION
-ORDER BY t.GENERATION;
+select count(*) as count, t.generation
+from ecoli_tree t
+left join ecoli_data d on t.id = d.parent_id
+where d.parent_id is null
+group by t.generation
+order by t.generation
